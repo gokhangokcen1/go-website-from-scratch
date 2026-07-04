eeffb6f7a7d5b1d5606d63eb39312ca7b3a6d09f
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2026-07-04 14:12:28 +0300

    Add files via upload

---
+++ gun-01-degiskenler-tipler-fonksiyonlar/tour-of-go/tour-of.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var i, j int = 1, 2
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
+
+	fmt.Println(add(42, 13))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println((split(17)))
+
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}
